cf27c23da9c114ae35aaf143c9aa96275e9599c7
gdiazi <guillermo.diazipa@gmail.com>
2023-01-29 23:45:17 -0500

    añadiendo backend

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '21f596d3e4a5c98cb2668810844df9bd'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '6c354fd9-ffce-41cb-b1b9-c8130ea96e29'
+};
